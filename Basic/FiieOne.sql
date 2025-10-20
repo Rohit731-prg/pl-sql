90edd5290b4b8c9c7c28c11d511b6557aa7023dc
Rohit731-prg <rohitsingha821380@gmail.com>
2025-10-20 11:07:02 +0530

    file1

---
+++ Basic/FiieOne.sql
@@ -0,0 +1,20 @@
+set serveroutput on
+
+prompt Enter Employee Id :
+accept N number
+
+declare
+    did emp.eid%type;
+    dname emp.name%type;
+    dage emp.age%type;
+begin
+    select eid, name, age 
+    into did, dname, dage
+    from emp
+    where eid = &N;
+
+    dbms_output.put_line('Employee id: ' || did);
+    dbms_output.put_line('Employee name: ' || dname);
+    dbms_output.put_line('Employee age: ' || dage);
+end;
+/
